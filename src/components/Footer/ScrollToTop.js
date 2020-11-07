e4425b5ed69ec010ae779c174e56f13b259c0c17
arc-coder <harsh@arccoder.in>
2020-11-07 18:08:42 +0530

    C6 Created Scroll to top button

--- src/components/Footer/ScrollToTop.js
+++ src/components/Footer/ScrollToTop.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ScrollToTop = () => {
-    return <div className="scroll_to_top__container">Scroll</div>;
+    return (
+        <div className="scroll_to_top__container">
+            <a href="#home" className="scroll_to_top">
+                <span>
+                    <i className="fas fa-chevron-up"></i>
+                </span>
+            </a>
+        </div>
+    );
 };
 
 export default ScrollToTop;

54c050fed7a27935cead99dac2f30d08a28593a1
arc-coder <harsh@arccoder.in>
2020-11-07 19:06:25 +0530

    C8 Created About Page

--- src/components/Body/About.js
+++ src/components/Body/About.js
@@ -3,7 +3,52 @@ import React from "react";
 const About = () => {
     return (
         <div className="about__container" id="about">
-            <div className="about__header">About Me</div>
+            <div className="about__header">
+                <div className="header">About Me</div>
+            </div>
+
+            <div className="about__content container">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Consequuntur esse pariatur optio odio laudantium libero
+                    quasi quisquam eum inventore tempore mollitia ut qui
+                    sapiente vero, debitis nesciunt corrupti repudiandae.
+                    Dolorem consectetur praesentium distinctio debitis rerum sit
+                    exercitationem vitae necessitatibus obcaecati? Placeat,
+                    natus aliquam sunt quisquam magni quam et saepe fuga ut,
+                    tenetur, illum deserunt culpa dolorum in ad earum nisi.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Consequuntur esse pariatur optio odio laudantium libero
+                    quasi quisquam eum inventore tempore mollitia ut qui
+                    sapiente vero, debitis nesciunt corrupti repudiandae.
+                    Dolorem consectetur praesentium distinctio debitis rerum sit
+                    exercitationem vitae necessitatibus obcaecati? Placeat,
+                    natus aliquam sunt quisquam magni quam et saepe fuga ut,
+                    tenetur, illum deserunt culpa dolorum in ad earum nisi.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Consequuntur esse pariatur optio odio laudantium libero
+                    quasi quisquam eum inventore tempore mollitia ut qui
+                    sapiente vero, debitis nesciunt corrupti repudiandae.
+                    Dolorem consectetur praesentium distinctio debitis rerum sit
+                    exercitationem vitae necessitatibus obcaecati? Placeat,
+                    natus aliquam sunt quisquam magni quam et saepe fuga ut,
+                    tenetur, illum deserunt culpa dolorum in ad earum nisi.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Consequuntur esse pariatur optio odio laudantium libero
+                    quasi quisquam eum inventore tempore mollitia ut qui
+                    sapiente vero, debitis nesciunt corrupti repudiandae.
+                    Dolorem consectetur praesentium distinctio debitis rerum sit
+                    exercitationem vitae necessitatibus obcaecati? Placeat,
+                    natus aliquam sunt quisquam magni quam et saepe fuga ut,
+                    tenetur, illum deserunt culpa dolorum in ad earum nisi.
+                </p>
+            </div>
         </div>
     );
 };

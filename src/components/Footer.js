e58756fe10910d664004ed6136dd82b16db52e93
arc-coder <harsh@arccoder.in>
2020-11-07 17:43:02 +0530

    C5 Created Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,43 @@ const Footer = () => {
     return (
         <div className="footer__container">
             <ScrollToTop />
-            <div className="footer__text">
-                <p>Footer</p>
+
+            <div className="text__container">
+                <div className="harsh__container">
+                    <div className="data__container">
+                        Made with{" "}
+                        <span>
+                            <i className="fas fa-heart"></i>
+                        </span>{" "}
+                        and{" "}
+                        <span>
+                            <i className="fas fa-mug-hot"></i>
+                        </span>{" "}
+                        by
+                    </div>
+                    <a
+                        href="https://github.com/arc-coder"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="link__container"
+                    >
+                        Arc Coder | Harsh Patel
+                    </a>
+                </div>
+
+                <hr />
+
+                <div className="radhika__container">
+                    <div className="data__container">Logo designed by</div>
+                    <a
+                        href="https://instagram.com/fashion_illustration98"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="link__container"
+                    >
+                        Radhika Somani
+                    </a>
+                </div>
             </div>
         </div>
     );

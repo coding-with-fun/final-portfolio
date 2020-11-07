2fc645725207309dc4af00be4a337c7ffdd57f15
arc-coder <harsh@arccoder.in>
2020-11-07 18:20:36 +0530

    C7 Created Home Page

--- src/components/Footer/ScrollToTop.js
+++ src/components/Footer/ScrollToTop.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const ScrollToTop = () => {
     return (
-        <div className="scroll_to_top__container">
-            <a href="#home" className="scroll_to_top">
+        <a href="#home" className="scroll_to_top">
+            <div className="scroll_to_top__container">
                 <span>
                     <i className="fas fa-chevron-up"></i>
                 </span>
-            </a>
-        </div>
+            </div>
+        </a>
     );
 };
 

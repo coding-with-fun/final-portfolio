9a1fe67a20b10c2a0eb29c2dc0390c06303541cc
arc-coder <harsh@arccoder.in>
2020-11-07 19:46:13 +0530

    C10 Minor Tweaks

---
+++ src/components/Body/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <div className="projects__container" id="projects">
+            <div className="projects__header">Projects</div>
+        </div>
+    );
+};
+
+export default Projects;
---
+++ src/components/Body/Skills.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Skills = () => {
+    return (
+        <div className="skills__container" id="skills">
+            <div className="skills__header">Skills</div>
+        </div>
+    );
+};
+
+export default Skills;
